575406fd8ec004c1fafc26857ed1991ff5ba6768
Ruchika Agarwal <ruchika.kharwar@gmail.com>
2015-04-13 09:53:23 +0000

    Added ArgParser

--- snippets.py
+++ snippets.py
@@ -1,4 +1,4 @@
-##!/usr/bin/python
+#!/usr/bin/python
 import logging
 
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
@@ -9,7 +9,7 @@ def put(name, snippet):
   Returns the name and snippet
   """
   logging.error("FIXME: Unimplemented - put({!r},{!r})".format(name, snippet))
-  
+                                                                                    
   
 def get(name):
   """ Retrieves the snippet with a given name.
@@ -18,6 +18,13 @@ def get(name):
   """  
   logging.error("FIXME: Unimplemented - get({!r})".format(name))
   return ""
-
-#if __name__ == '__main__':
-#  put(name, snippet)
+import argparse
+import sys 
+def main():
+  """Main function"""
+  logging.info("Construction of parser")
+  parser = argparse.ArgumentParser(description = "Store and retrieve snippets of text ")
+  arguments = parser.parse_args(sys.argv[1:])
+  print "Arguments is {}".format(arguments)
+if __name__ == '__main__':  
+  main()
